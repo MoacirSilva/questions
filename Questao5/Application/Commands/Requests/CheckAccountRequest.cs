94538dec47cac356e9e846b5bc43a25e9b67aba9
Moacir Pereira da Silva <moacirsilva.it@gmail.com>
2024-07-04 16:12:09 -0300

    create queries and commands

---
+++ Questao5/Application/Commands/Requests/CheckAccountRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Questao5.Application.Commands.Responses;
+
+namespace Questao5.Application.Commands.Requests
+{
+    public class CheckAccountRequest: IRequest<CheckAccountResponse>
+    {
+        public string MoveType { get; set; }
+        public int Number { get; set; }
+        public decimal Value { get; set; }
+        public CheckAccountRequest() { }
+    }
+}
---
+++ Questao5/Application/Queries/Responses/BalanceViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Questao5.Application.Queries.Responses
+{
+    public class BalanceViewModel
+    {
+        public string Saldo { get; set; } = string.Empty;
+        public int Numero { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string DataHora { get; set; }
+
+        public BalanceViewModel()
+        {
+            DataHora = DateTime.Now.ToString();
+        }
+    }
+}
